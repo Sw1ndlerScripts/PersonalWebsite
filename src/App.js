6e27a554b0bc06f50c818e9db296c95fd1321889
Sw1ndler <lisina2023@icloud.com>
2022-09-10 23:46:54 -0700

    Website using react instead of svelte

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [word, setWord] = useState("");
+
+  const timer = (ms) => new Promise((res) => setTimeout(res, ms));
+
+  useEffect(() => {
+    let interval = 0;
+    const whoAmI = ["", "a coding enthusiast", "a lua developer", "Alex"];
+    function typeWords() {
+      if (interval < whoAmI.length - 1) {
+        let curWord = "";
+        interval += 1;
+
+        async function typeWord() {
+          for (const letter of whoAmI[interval].split("")) {
+            curWord = curWord + letter;
+            setWord(curWord);
+
+            await timer(50);
+          }
+
+          if (interval <= whoAmI.length - 2) {
+            await timer(1000);
+
+            let wordList = curWord.split("");
+            const looptimes = wordList.length;
+
+            for (let i = 0; i < looptimes; i++) {
+              wordList.pop();
+              setWord(wordList.join(""));
+              await timer(50);
+            }
+          }
+        }
+        typeWord();
+      }
+    }
+
+    typeWords();
+    setInterval(typeWords, 3200);
+  }, [setWord]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <img
+        className="pfp"
+        src="https://cdn.discordapp.com/attachments/822525621283454996/1018328604544864436/unknown.png"
+        alt="pfp"
+      />
+
+      <h1 className="heyThere">Hey There!</h1>
+
+      <code className = "im">
+        I'm {" "} {word}
+        <span className="cursor">|</span>
+      </code>
+
+    </>
   );
 }
 
