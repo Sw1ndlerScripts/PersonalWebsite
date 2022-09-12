0dc746412ae2da78d1bb9ed0c78c1082ed06b8b7
Sw1ndler <lisina2023@icloud.com>
2022-09-12 15:42:44 -0700

    Added animation to text

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
 
   const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
+
+
+
   useEffect(() => {
     let interval = 0;
     const whoAmI = ["", "a coding enthusiast", "a lua developer", "Alex"];
@@ -18,7 +21,14 @@ function App() {
           for (const letter of whoAmI[interval].split("")) {
             curWord = curWord + letter;
             setWord(curWord);
+            if (curWord === "Alex") {
 
+              setTimeout(() => {
+                const item = document.getElementsByClassName("imWord")[0]
+                item.classList.add("Animate")
+              }, 50);
+
+            }
             await timer(50);
           }
 
@@ -43,22 +53,31 @@ function App() {
     setInterval(typeWords, 3200);
   }, [setWord]);
 
+
+
+
+
+
   return (
-    <>
+    <div className="main">
       <img
         className="pfp"
         src="https://cdn.discordapp.com/attachments/822525621283454996/1018328604544864436/unknown.png"
         alt="pfp"
       />
 
+  
       <h1 className="heyThere">Hey There!</h1>
 
       <code className = "im">
-        I'm {" "} {word}
+        I'm&nbsp;
+
+        <span className="imWord" > {word} </span>
+
         <span className="cursor">|</span>
       </code>
 
-    </>
+    </div>
   );
 }
 
